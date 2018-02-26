778f4a92d6416d4a41c69ca78690addd468ae857
Hector Ramos <hramos@fb.com>
2018-02-26 14:38:40 -0800

    Do not close if template is missing

--- lib/iceboxer/template.rb
+++ lib/iceboxer/template.rb
@@ -15,7 +15,7 @@ module Iceboxer
       # close issues that lack a template
       closers.each do |closer|
         issues = Octokit.search_issues(closer[:search])
-        puts "[TEMPLATE] Found #{issues.items.count} issues to close in #{@repo} ..."
+        puts "[TEMPLATE] Found #{issues.items.count} issues to nag in #{@repo} ..."
         issues.items.each do |issue|
           unless already_nagged?(issue.number)
             puts "Nagging https://github.com/#{@repo}/issues/#{issue.number}: #{issue.title}"
@@ -30,13 +30,12 @@ module Iceboxer
       [
         {
           # The latest template, with "Is this a bug report?", was introduced on June 27, 2017.
-          :search => "repo:#{@repo} is:issue is:open created:>=2017-06-27 NOT \"Is this a bug report?\" in:body NOT \"cherry-pick\" in:title -label:\"For Discussion\" comments:<5 -label:\"Core Team\" -label:\"Documentation\" -label:\"Missing required information from template\" -label:\"Needs more information\" -label:\"For Stack Overflow\" -label:\"Icebox\" -label:\"Good First Task\" NOT \"relicense\" in:title"
+          :search => "repo:#{@repo} is:issue is:open created:>=2017-06-27 NOT \"Is this a bug report?\" in:body NOT \"cherry-pick\" in:title -label:\"For Discussion\" comments:<5 -label:\"Core Team\" -label:\"Documentation\" -label:\"Missing required information from template\" -label:\"Needs more information\" -label:\"For Stack Overflow\" -label:\"Icebox\" -label:\"Good First Task\" -label:\"no-template\""
         },
         {
           # Earlier than July, let's check if they at least have some sort of repro steps
-          :search => "repo:#{@repo} is:issue is:open created:<2017-06-27 NOT \"reproduction\" in:body NOT \"cherry-pick\" in:title -label:\"For Discussion\" comments:<5 -label:\"Core Team\" -label:\"Documentation\" -label:\"Missing required information from template\" -label:\"Needs more information\" -label:\"For Stack Overflow\" -label:\"Icebox\" -label:\"Good First Task\" NOT \"relicense\" in:title"
+          :search => "repo:#{@repo} is:issue is:open created:<2017-06-27 NOT \"reproduction\" in:body NOT \"cherry-pick\" in:title -label:\"For Discussion\" comments:<5 -label:\"Core Team\" -label:\"Documentation\" -label:\"Missing required information from template\" -label:\"Needs more information\" -label:\"For Stack Overflow\" -label:\"Icebox\" -label:\"Good First Task\""
         }
-
       ]
     end
 
@@ -48,16 +47,16 @@ module Iceboxer
     def templateNag(issue, reason)
       Octokit.add_comment(@repo, issue, message("there"))
       Octokit.update_issue(@repo, issue, :labels => ["no-template", "Ran Commands"])
-      Octokit.close_issue(@repo, issue)
+      # Octokit.close_issue(@repo, issue)
 
       puts "Template nagged #{@repo}/issues/#{issue}!"
     end
 
-    def message(issue_author)
+    def message(reason)
       <<-MSG.strip_heredoc
-      Thanks for posting this! It looks like your issue is missing some required information. Can you please add all the details specified in the [Issue Template](https://raw.githubusercontent.com/facebook/react-native/master/.github/ISSUE_TEMPLATE.md)? This is necessary for people to be able to understand and reproduce your issue. 
+      Thanks for posting this! It looks like your issue may be missing some required information. Are all the fields required by the [Issue Template](https://raw.githubusercontent.com/facebook/react-native/master/.github/ISSUE_TEMPLATE.md) filled out?
       
-      I am going to close this, but please feel free to open a new issue with the additional information provided. Thanks!
+      This is a friendly reminder and you may safely ignore this message if you believe your issue contains all the relevant information. Thank you for your contributions.
       
       <sub>[How to Contribute](https://facebook.github.io/react-native/docs/contributing.html#bugs) • [What to Expect from Maintainers](https://facebook.github.io/react-native/docs/maintainers.html#handling-issues)</sub>
 
