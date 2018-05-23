9ab2b1c7eee346f3899bd4bfdb851c14b9685b6a
Hector Ramos <hramos@fb.com>
2018-05-23 10:15:50 -0700

    Update template link

--- lib/bot/template.rb
+++ lib/bot/template.rb
@@ -121,7 +121,7 @@ module Bot
           "nag_reason": "no-template"
         }
       -->
-      It looks like your issue may be incomplete. Are all the fields required by the [Issue Template](https://raw.githubusercontent.com/facebook/react-native/master/.github/ISSUE_TEMPLATE.md) filled out?
+      It looks like your issue may be incomplete. Are all the fields required by the [Issue Template](https://raw.githubusercontent.com/facebook/react-native/master/.github/ISSUE_TEMPLATE/bug_report.md) filled out?
 
       If you believe your issue contains all the relevant information, let us know in order to have a maintainer remove the `No Template` label.
       MSG
