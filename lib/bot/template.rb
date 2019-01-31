17ba3c1852eafedb8b982acd7d13e44b39452800
Héctor Ramos <hector@hectorramos.com>
2019-01-31 15:51:30 -0800

    Close issues with the no template label

--- lib/bot/template.rb
+++ lib/bot/template.rb
@@ -19,6 +19,7 @@ module Bot
       @label_docs = "Type: Docs"
       @label_good_first_issue = "Good first issue"
       @label_tests = "🔩Test Infrastructure"
+      @label_ran_commands = "Ran Commands"
     end
 
     def perform
@@ -37,59 +38,40 @@ module Bot
         {
           :search => "repo:#{@repo} is:open is:issue -label:\"#{@label_for_discussion}\" -label:\"#{@label_core_team}\" -label:\"#{@label_customer}\" -label:\"#{@label_ci_test_failure}\" -label:\"#{@label_bug_report}\" -label:\"#{@label_docs}\" -label:\"#{@label_for_stack_overflow}\" -label:\"#{@label_good_first_issue}\" -label:\"#{@label_no_template}\" -label:\"#{@label_tests}\" -label:\"#{@label_ci_test_failure}\" created:>=2019-01-26",
           :action => 'close_template'
+        },
+        {
+          :search => "repo:#{@repo} is:open is:issue -label:\"#{@label_core_team}\" -label:\"#{@label_customer}\" label:\"#{@label_no_template}\" created:>=2019-01-26",
+          :action => 'close_template'
         }
+
       ]
     end
 
     def process(issue, candidate)
-      if candidate[:action] == 'label_for_discussion'
-        label_for_discussion(issue)
-      end
       if candidate[:action] == 'close_template'
         close_template(issue)
-      end
-      if candidate[:action] == 'remove_template_label'
-        remove_template_label(issue)
-      end
-      if candidate[:action] == 'close_question'
-        close_question(issue)
       end
     end
 
     def already_nagged?(issue)
       comments = Octokit.issue_comments(@repo, issue)
-      comments.any? { |c| c.body =~ /Issue Template/ }
-    end
-
-    def label_for_discussion(issue)
-      labels = [@label_for_discussion];
-      add_labels(issue, labels)
+      comments.any? { |c| c.body =~ /issue template/ }
     end
 
     def close_template(issue)
       labels = [@label_no_template];
 
-      return if issue_contains_label(issue, @label_for_discussion)
       return if issue_contains_label(issue, @label_core_team)
+      return if issue_contains_label(issue, @label_customer)
 
       unless already_nagged?(issue.number)
         Octokit.add_comment(@repo, issue.number, message)
       end
 
       Octokit.close_issue(@repo, issue.number)
-      add_labels(issue, ["Ran Commands"])
+      labels.push @label_ran_commands
+      add_labels(issue, labels)
       puts "#{@repo}: ️[TEMPLATE] ❗📋  #{issue.html_url}: #{issue.title} -> Missing template, closed"
-
-      add_labels(issue, labels)
-    end
-
-    def close_question(issue)
-      labels = [@label_for_stack_overflow];
-      add_labels(issue, labels)
-    end
-
-    def remove_template_label(issue)
-      remove_label(issue, @label_no_template)
     end
 
     def add_labels(issue, labels)
@@ -134,9 +116,7 @@ module Bot
       -->
       We are automatically closing this issue because it does not appear to follow any of the provided [issue templates](https://github.com/facebook/react-native/issues/new/choose).
 
-      Please make use of the [bug report template](https://github.com/facebook/react-native/issues/new?template=bug_report.md) to let us know about a **reproducible bug or regression** in the **core** React Native library.
-
-      If you'd like to propose a change or discuss a feature request, there is a repository dedicated to [Discussions and Proposals](https://github.com/react-native-community/discussions-and-proposals) you may use for this purpose.
+      👉 [Click here if you want to report a reproducible bug or regression in React Native.](https://github.com/facebook/react-native/issues/new?template=bug_report.md)
       MSG
     end
   end
